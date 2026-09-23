655dad3552e0ede1f2a682e5660874f92b3d9593
nicliszman-cloud <nic.liszman@gmail.com>
2026-09-23 11:42:01 -0300

    Serve legal pages from Supabase

--- lib/legal.ts
+++ lib/legal.ts
@@ -1,3 +1,3 @@
-export const PRIVACY_POLICY_URL = 'https://nicliszman-cloud.github.io/project-apex/privacy.html';
-export const ACCOUNT_DELETION_URL = 'https://nicliszman-cloud.github.io/project-apex/delete-account.html';
-export const PRIVACY_CONTACT_URL = 'https://nicliszman-cloud.github.io/project-apex/privacy-contact.html';
+export const PRIVACY_POLICY_URL = 'https://qbfkietvmhgzgztjxxdw.supabase.co/functions/v1/privacy-policy';
+export const ACCOUNT_DELETION_URL = 'https://qbfkietvmhgzgztjxxdw.supabase.co/functions/v1/account-deletion';
+export const PRIVACY_CONTACT_URL = 'https://qbfkietvmhgzgztjxxdw.supabase.co/functions/v1/privacy-contact-page';
